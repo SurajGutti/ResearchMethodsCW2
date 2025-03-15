9cfa72f19ddc3580e0670d3c9ad694f3b930f648
Suraj Gutti <surajgutti1993@gmail.com>
2025-03-15 11:32:55 +0000

    added bio to script

--- dietgroups_female.py
+++ dietgroups_female.py
@@ -5,11 +5,8 @@ diet_groups = {}
 mean_ghg = {}
 average_ghg = {}
 
-mean_ch4 = {}
-average_ch4 = {}
-
-mean_n2o = {}
-average_n2o = {}
+mean_bio = {}
+average_bio = {}
 
 mean_land = {}
 average_land = {}
@@ -35,20 +32,15 @@ for index, row in results.iterrows():
         else:
             diet_groups[row.diet_group] = 1
 
+        if row.diet_group in mean_bio:
+            mean_bio[row.diet_group] += row.mean_bio
+        else:
+            mean_bio[row.diet_group] = row.mean_bio
+
         if row.diet_group in mean_ghg:
             mean_ghg[row.diet_group] += row.mean_ghgs
         else:
             mean_ghg[row.diet_group] = row.mean_ghgs
-
-        if row.diet_group in mean_ch4:
-            mean_ch4[row.diet_group] += row.mean_ghgs_ch4
-        else:
-            mean_ch4[row.diet_group] = row.mean_ghgs_ch4
-
-        if row.diet_group in mean_n2o:
-            mean_n2o[row.diet_group] += row.mean_ghgs_n2o
-        else:
-            mean_n2o[row.diet_group] = row.mean_ghgs_n2o
 
         if row.diet_group in mean_land:
             mean_land[row.diet_group] += row.mean_land
@@ -77,17 +69,16 @@ for index, row in results.iterrows():
 
 for group, value in diet_groups.items():
     average_ghg[group] = mean_ghg[group] / diet_groups[group]
-    average_ch4[group] = mean_ch4[group] / diet_groups[group]
-    average_n2o[group] = mean_n2o[group] / diet_groups[group]
+    average_bio[group] = mean_bio[group] / diet_groups[group]
     average_land[group] = mean_land[group] / diet_groups[group]
     average_watscar[group] = mean_watscar[group] / diet_groups[group]
     average_eut[group] = mean_eut[group] / diet_groups[group]
     average_watuse[group] = mean_watuse[group] / diet_groups[group]
     average_acid[group] = mean_acid[group] / diet_groups[group]
 
-env_impact = average_ghg, average_ch4, average_n2o, average_land, average_watscar, average_eut, average_watuse, average_acid
+env_impact = average_ghg, average_bio, average_land, average_watscar, average_eut, average_watuse, average_acid
 
 df = pandas.DataFrame(env_impact)
-df.insert(0, "Impact Type", ['GreenHouseGas', 'CH4', 'N2O', 'LandUsed', 'WaterScarcity', 'Eutrophication', 'WaterUse', 'Acid'])
+df.insert(0, "Impact Type", ['GreenHouseGas', 'Bio', 'LandUsed', 'WaterScarcity', 'Eutrophication', 'WaterUse', 'Acid'])
 
 df.to_excel('diet_groups_female.xlsx', sheet_name='female', index=False)
